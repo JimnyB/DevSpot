2fa9483fad4481809ef488acd5313e8ff9acc2f1
Jimmy Barr <jimmy.barr@sigma.se>
2025-02-06 09:52:35 +0100

    Initial commit

---
+++ DevSpot/Data/RoleSeeder.cs
@@ -0,0 +1,28 @@
+﻿using DevSpot.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevSpot.Data
+{
+    public class RoleSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (!roleManager.RoleExistsAsync(Roles.Admin).Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+            }
+
+            if (!roleManager.RoleExistsAsync(Roles.JobSeeker).Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.JobSeeker));
+            }
+
+            if (!roleManager.RoleExistsAsync(Roles.Empolyer).Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.Empolyer));
+            }
+        }
+    }
+}
